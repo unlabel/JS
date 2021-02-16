eda6c5182ccc83b87a5ec779edb437aed14eb0e2
Denis <unlabel@yandex.ru>
2021-02-16 21:39:15 +0300

    Создание формы запроса

---
+++ js/scrirt.js
@@ -0,0 +1,10 @@
+const numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?");
+console.log(numberOfFilms);
+
+let personalMovieDB = {
+    count: "numberOfFilms",
+    movies: " ",
+    actors: " ",
+    genres: " ",
+    privat: Boolean,
+};
